d1d6b109b7568932a9f3afae102c13b8c6d889c0
Min Kaung Kyaw <minkaungkyaw@yomafleet.com>
2026-02-21 21:00:12 +0630

    chore: modify rule

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,8 +5,6 @@ module.exports = {
     "subject-case": [0],
     // not allow to use more than 100 characters in commit message
     "header-max-length": [2, "always", 100],
-    // not allow full stop in commit message
-    "subject-full-stop": [2, "never", "."],
     // not allow empty subject
     "subject-empty": [2, "never"],
     "type-empty": [2, "never"],
